839189eef1face2b1173a8153dd058d420331df9
HectorRodrigo450 <hrodrigolopezm117@gmail.com>
2025-03-24 22:51:46 -0600

    Crear practica 1
    practica 1

---
+++ Practicas/js/practica_4.js
@@ -0,0 +1,9 @@
+function funcion1(){
+	document.getElementById("idImagen").src = "https://media.istockphoto.com/id/1141529240/es/vector/simple-manzana-en-estilo-plano-ilustraci%C3%B3n-vectorial.jpg?s=612x612&w=0&k=20&c=LVaXkw5udL6sGP5cQT7PHq122aaZrYs2RUDwk7-hOh8=";
+	document.getElementById("idTexto").innerText = "Manzana";
+}
+
+function funcion2(){
+	document.getElementById("idImagen").src = "https://www.recetasnestle.com.mx/sites/default/files/inline-images/tipos-de-manzana-royal-gala.jpg";
+	document.getElementById("idTexto").innerText = "Manzana real";
+}
